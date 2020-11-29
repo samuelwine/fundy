29f7407cf02127bc02f9ef79da74af0ceac6afaf
shmuel <samuelwine@gmail.com>
2020-11-29 16:55:54 +0000

    change to sql and update nuget packages to net 5

--- src/Fundy.Infrastructure/StartupSetup.cs
+++ src/Fundy.Infrastructure/StartupSetup.cs
@@ -7,16 +7,10 @@ namespace Fundy.Infrastructure
 {
 	public static class StartupSetup
 	{
-		public static void AddDevelopmentDbContext(this IServiceCollection services, string connectionString)
+		public static void AddDbContext(this IServiceCollection services, string connectionString)
         {
 				services.AddDbContext<AppDbContext>(options =>
 				options.UseSqlServer(connectionString)); // will be created in web project root
-		}
-
-		public static void AddProductionDbContext(this IServiceCollection services, string accountEndpoint, string accountKey, string databaseName)
-		{
-			services.AddDbContext<AppDbContext>(options =>
-			options.UseCosmos(accountEndpoint, accountKey, databaseName)); 
 		}
 	}
 }
